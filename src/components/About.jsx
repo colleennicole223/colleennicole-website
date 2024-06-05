d9be69c35f83300ae8a2c3856373f7fa8d9b40ba
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-06-05 16:17:17 -0400

    Updated content in About me and Project Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
             <div className="aboutContent">
             <h2>About Me</h2>
             <p>
-            My name is Colleen! Some words that describe me are; problem solver, quite friendly, and very organized. While I am an engineer, 
-            I have a knack for design and love when a product balances form and function. I am starting a <span className="highlight">Master 
-            of Computer Science</span> in the fall of 2024, specializing in human-computer interaction. I am <span className="highlight">actively 
-            searching for a full-time role</span> to kick off my career in software. 
+            Hello, my name is Colleen! Some words that describe me are: problem solver, quite friendly, and very organized. While I am an engineer, I have a knack for design and love when a product balances form and function. I am starting a
+            <span className="highlight"> Master of Computer Science </span> 
+            in the fall of 2024, specializing in human-computer interaction. I am
+            <span className="highlight"> actively searching for a full-time role </span> 
+            to kick off my career in software.
             </p>
             <p>
-            Outside of the professional world, I enjoy outdoor activities with my dogs, digital art, 3D modeling, and video games. Thank you 
-            for taking the time to explore my portfolio. Do you have an opportunity in mind or just want to say hi? 
+            Outside of the professional world, I enjoy outdoor activities with my dogs, digital art, 3D modeling, and video games. Thank you for taking the time to explore my portfolio. Do you have an opportunity in mind or just want to say hi? 
             </p>
             {/* <div className='socialIcons'>
                 <div className='socialIcon'></div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,12 +11,19 @@ const Projects = () => {
     <section id="projects" >
       <div className="section-content">
         <h2>Projects</h2>
+        <p>Welcome to the project section of my portfolio. Below is a collection of my most recent projects, each presenting unique challenges and showcase my ability to solve complex problems. </p>
         <div className="projectTiles">
           <div className="tile">
             <img className="tileImg" src={project2} />
             <div className="tileContent">
               <h4>Writer Gadget</h4>
-              <p>I am reworking the front end of Writers Gadget, a responsive application to plan, organize, and track story ideas. I am utilizing <span className="highlight">Figma</span> and familiarizing myself with <span className="highlight">Flutter</span> to implement an intuitive and user-friendly interface. </p>
+              <p>
+                I am reworking the front end of Writers Gadget, a responsive application designed to plan, organize, and track story ideas. Utilizing  
+                <span className="highlight"> Figma </span> 
+                for design and familiarizing myself with 
+                <span className="highlight"> Flutter</span> 
+                , I aim to implement an intuitive and user-friendly interface for this functional application. 
+              </p>
               <a className="link" href="https://play.google.com/store/apps/details?id=com.writergadget.writer_gadget&hl=en_US" target="_blank" rel="noopener noreferrer">
                 <img  src={link} />
                 <h5 className='highlight'>Google Play</h5>
@@ -35,7 +42,14 @@ const Projects = () => {
             <img className="tileImg" src={project3} />
             <div className="tileContent">
               <h4>Thymer</h4>
-              <p>I am developing a time management application, Thymer. This responsive application will combine a to-do list, habit tracker, and Pomodoro timer. This app is actively being designed with <span className="highlight">Figma</span> and is projected to be built with <span className="highlight">React</span> and <span className="highlight">Node.js</span>. </p>
+              <p>
+                I am developing Thymer, a time management application, that combines a to-do list, habit tracker, and Pomodoro timer. This responsive application is currently being designed with
+                <span className="highlight"> Figma </span> 
+                and is projected to be built with 
+                <span className="highlight"> React </span> 
+                and 
+                <span className="highlight"> Node.js </span>. 
+              </p>
               {/* <h5>Developing a productivity application aimed to help users manage their tasks, set goals, and 
                 track progress efficiently. </h5> */}
               {/* <div className="projectSkill">
