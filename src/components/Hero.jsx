27b12aefc5a3872d9f8e0195ab3b15bb9ac81067
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-06-18 17:17:10 -0400

    Updated model reference based on light or dark mode preference

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 // import video1 from '../video-1.mp4';
 import Spline from '@splinetool/react-spline';
+import getColor from 'use-prefers-color-scheme';
 
 const Hero = () => {
 
-  
+  const color = getColor();
 
   return (
     <>
@@ -18,7 +19,7 @@ const Hero = () => {
           <h3>PROJECT COORDINATOR</h3>
           <h1>FULTON</h1>
           </div>
-          <Spline scene="https://prod.spline.design/2OJTcEHcxFIuNNt1/scene.splinecode" />
+          <Spline scene={color === "dark" ? "https://prod.spline.design/2OJTcEHcxFIuNNt1/scene.splinecode" : "https://prod.spline.design/kvpHO2CoNhBLwFhh/scene.splinecode"}/>
         {/* <h4>welcome to my portfolio</h4> */}
       <div className="heroFooter"></div> 
     </section>
