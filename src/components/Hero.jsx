7382316dbef485a14c4e58dfc082d51b9877fae2
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-06-25 20:02:13 -0400

    Updated background and 3D model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import { React } from 'react'
 import Spline from '@splinetool/react-spline';
 import getColor from 'use-prefers-color-scheme';
 
 const Hero = () => {
 
   const color = getColor();
-
   return (
     <>
     <section id="home">
-        <div className='heroContent'>
-          <h1 >COLLEEN</h1>
-          <h3>DESIGNER, ENGINEER, &</h3>
-          <h3>PROJECT COORDINATOR</h3>
-          <h1>FULTON</h1>
-          </div>
-          <Spline scene={color === "dark" ? "https://prod.spline.design/2OJTcEHcxFIuNNt1/scene.splinecode" : "https://prod.spline.design/kvpHO2CoNhBLwFhh/scene.splinecode"}/>
+        <div className='section-content'>
+          <div></div>
+          {/* <h2>Hello, my name is</h2>
+          <h1>Colleen</h1> */}
+          
+          <Spline 
+            scene={color === "dark" ? "https://prod.spline.design/fXLZ73mK6pTQAzB5/scene.splinecode" : "https://prod.spline.design/vy39nqah9k40oSvF/scene.splinecode"} 
+            onLoad={(spline) => {
+              if(window.innerWidth<1200){
+                spline.setZoom(window.innerWidth/400); }else{
+                  spline.setZoom(2)
+                }
+            }}
+            />
+            </div>
     </section>
     </>
   )
